6adee01da3328228603b6d8704349fd8169e8565
norstein21 <norstein4me@hotmail.co.id>
2019-05-14 11:36:47 +0700

    Initial Commit

---
+++ duainput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  duainput
+//
+//  Created by norstein21 on 14/05/19.
+//  Copyright © 2019 fntstic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
